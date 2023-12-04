0af5e9ab750105e9e02d98c4f8656ab87b7bba40
Anastasiya Zdanovich <96586999+NastyaZdanovich@users.noreply.github.com>
2023-12-04 15:14:31 +0300

    Delete second_selenium_practice directory

--- second_selenium_practice/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture
-def driver():
-    options = webdriver.ChromeOptions()
-    with webdriver.Chrome(options) as driver:
-        yield driver
